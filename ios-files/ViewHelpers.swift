4321b1ae3953eb095c685de0449c0a2dc4f6d471
Jasper Wu <jasper.pwu@gmail.com>
2025-10-03 00:57:33 -0700

    fix syntax

--- ios-files/ViewHelpers.swift
+++ ios-files/ViewHelpers.swift
@@ -3,7 +3,6 @@ import SwiftUI
 func resizableImage(imageName: String) -> some View {
   Image.dynamic(assetNameOrPath: imageName)
     .resizable()
-    .scaledToFit()
-    .background(Color.white)
-    .cornerRadius(8)
+    .aspectRatio(contentMode: .fit)
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
 }
